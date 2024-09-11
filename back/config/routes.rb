deb3084528cd6504eef59f7b5e86a04ba018900c
topi0247 <karubona.ra247@gmail.com>
2024-09-11 23:02:24 +0900

    feat:バックのログイン認証追加

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback', to: 'api/v1/sessions#create'
+  namespace :api do
+    namespace :v1 do
+      root 'bases#index'
+      get 'current_user', to: 'sessions#current_user'
+    end
+  end
 end
