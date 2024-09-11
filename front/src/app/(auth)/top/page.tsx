36cbc1299ba2e1a6773bb4702e0dc48811595fed
topi0247 <karubona.ra247@gmail.com>
2024-09-12 02:00:58 +0900

    feat:検索フォームガワ作成

--- front/src/app/(auth)/top/page.tsx
+++ front/src/app/(auth)/top/page.tsx
@@ -1,7 +1,28 @@
+import { MdSearch } from "rocketicons/md";
+
 export default function Top() {
   return (
-    <article>
-      <h1>トップページ</h1>
+    <article className="flex w-full flex-col items-center justify-center gap-2">
+      <div className="w-full text-start font-bold">
+        <h1>ソーシャルポートフォリオ</h1>
+      </div>
+      <div className="flex w-full items-center">
+        <div className="has_select">
+          <select>
+            <option>アプリ</option>
+            <option>記事</option>
+            <option>受講生</option>
+          </select>
+        </div>
+        <input
+          type="text"
+          placeholder="アプリを探す"
+          className="w-full rounded-l border border-[#ccc] bg-white px-4 py-2 text-sm"
+        />
+        <button className="rounded-r border bg-runteq-primary px-3 py-1.5">
+          <MdSearch className="icon-white-lg" />
+        </button>
+      </div>
     </article>
   );
 }
